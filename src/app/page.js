8b160ca1d7bc398eea7e1dc536444b00f9a8e20a
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-11-11 20:23:01 +0530

    handled the error

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ?
     "movie/top_rated" : "trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`,
+    }?api_key=a${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
 
@@ -20,6 +20,6 @@ export default async function Home({ searchParams }) {
   return (
     <div>
       <Results results={results} />
-    </div >
+    </div>
   )
 }
