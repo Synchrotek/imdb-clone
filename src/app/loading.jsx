f1de7f7ea9971f324650294825315d68a43812cd
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-11-11 20:37:37 +0530

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function loading() {
+    return (
+        <div className="flex justify-center">
+            <img className="h-96"
+                src="loading.svg" alt="loading..." />
+        </div>
+    )
+}
